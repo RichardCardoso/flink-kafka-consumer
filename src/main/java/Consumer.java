afbd7ed57b236a490c69d11bacf1edef9a98384c
Richard Cardoso <richard.166@hotmail.com>
2023-07-21 15:23:35 -0300

    feat: dynamic host and port

--- src/main/java/Consumer.java
+++ src/main/java/Consumer.java
@@ -1,13 +1,26 @@
+import org.apache.commons.lang3.StringUtils;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.connector.kafka.source.KafkaSource;
 import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
 
+import java.util.Map;
+
 public class Consumer {
 
-    public static KafkaSource<String> consumer() {
+    public static KafkaSource<String> consumer(Map<String, String> params) {
+
+        String host = "kafka";
+        String port = "29092";
+
+        if (!StringUtils.isEmpty(params.get("host"))) {
+            host = params.get("host");
+        }
+        if (!StringUtils.isEmpty(params.get("port"))) {
+            port = params.get("port");
+        }
 
         return KafkaSource.<String>builder()
-                .setBootstrapServers("kafka:29092")
+                .setBootstrapServers(host + ":" + port)
                 .setTopics("test")
                 .setGroupId("flink-test")
                 .setStartingOffsets(OffsetsInitializer.latest())
